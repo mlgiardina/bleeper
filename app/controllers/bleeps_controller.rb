318ad17cd60981df811c292e19f67c58dea24f3a
Matthew Giardina <mlgiardina@me.com>
2015-06-10 20:25:08 -0500

    added controllers

---
+++ app/controllers/bleeps_controller.rb
@@ -0,0 +1,2 @@
+class BleepsController < ApplicationController
+end
---
+++ test/controllers/bleeps_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BleepsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
